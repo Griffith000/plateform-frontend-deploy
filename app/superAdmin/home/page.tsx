3b854fa80b993d8e9ed6dbb5925cd3e70cb42b7f
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2025-01-05 00:18:22 +0100

    testing : assignments in admin part 5

--- app/superAdmin/home/page.tsx
+++ app/superAdmin/home/page.tsx
@@ -1,18 +1,16 @@
 'use client'
 
 import Layout from '@/mic-component/Admin_UI/Layout/Layout'
-import React, { useEffect, useState } from 'react'
-
-import { Box, Grid, Modal, Typography } from '@mui/material'
+import React from 'react'
 
 import { useAssignmentStore } from '@/store/MyStore/AssignmentsStore'
 import { Assignment } from '@/store/Models/Assignment'
-import AssignmentCard from '@/mic-component/assignment_UI/AssignmentCard'
 import Empty from '@/mic-component/lottie_animation/Empty'
 
 export default function Page() {
   const id_dep = '670792e3ee0e13424434d371'
   const fetchAssignments = useAssignmentStore(state => state.fetchAssignments)
+
   // const assignments: Assignment[] = useAssignmentStore(
   //   state => state.assignments
   // )
@@ -69,9 +67,9 @@ export default function Page() {
       <div style={{ marginTop: '20px', padding: '10px' }}>
         {assignments.length > 0 ? (
           assignments.map(assignment => (
-            <>
+            <div key={assignment._id}>
               <div>{assignment.Title}</div>
-            </>
+            </div>
           ))
         ) : (
           <div className='flex h-full flex-col items-center justify-center'>
