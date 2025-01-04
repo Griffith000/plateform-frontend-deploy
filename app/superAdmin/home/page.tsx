5d1a8604f703e896cff648a2ee2c135bd5862062
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2025-01-05 00:35:21 +0100

    testing : assignments in admin part dynamic

--- app/superAdmin/home/page.tsx
+++ app/superAdmin/home/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import Layout from '@/mic-component/Admin_UI/Layout/Layout'
-import React from 'react'
+import React, { useEffect } from 'react'
 
 import { useAssignmentStore } from '@/store/MyStore/AssignmentsStore'
 import { Assignment } from '@/store/Models/Assignment'
@@ -11,56 +11,56 @@ export default function Page() {
   const id_dep = '670792e3ee0e13424434d371'
   const fetchAssignments = useAssignmentStore(state => state.fetchAssignments)
 
-  // const assignments: Assignment[] = useAssignmentStore(
-  //   state => state.assignments
-  // )
+  const assignments: Assignment[] = useAssignmentStore(
+    state => state.assignments
+  )
 
-  // useEffect(() => {
-  //   const loadAssignments = async (departmentId: string) => {
-  //     await fetchAssignments(departmentId)
-  //   }
-  //   if (id_dep) {
-  //     loadAssignments(id_dep)
-  //   }
-  // }, [])
-
-  const assignments: Assignment[] = [
-    {
-      _id: '1',
-      Title: 'Math Homework',
-      Description: 'Complete exercises 1 to 10 from chapter 3.',
-      DueDate: '2025-01-15',
-      Attachments: 'math_homework.pdf'
-    },
-    {
-      _id: '2',
-      Title: 'Science Project',
-      Description: 'Prepare a presentation on the solar system.',
-      DueDate: '2025-01-20',
-      Attachments: 'science_project.zip'
-    },
-    {
-      _id: '3',
-      Title: 'History Essay',
-      Description: 'Write an essay on the Industrial Revolution.',
-      DueDate: '2025-01-18',
-      Attachments: 'history_essay.docx'
-    },
-    {
-      _id: '4',
-      Title: 'English Literature Analysis',
-      Description: "Analyze the themes in 'To Kill a Mockingbird'.",
-      DueDate: '2025-01-22',
-      Attachments: 'literature_analysis.docx'
-    },
-    {
-      _id: '5',
-      Title: 'Physics Lab Report',
-      Description: "Submit the lab report on Newton's Laws of Motion.",
-      DueDate: '2025-01-25',
-      Attachments: 'physics_lab_report.pdf'
+  useEffect(() => {
+    const loadAssignments = async (departmentId: string) => {
+      await fetchAssignments(departmentId)
     }
-  ]
+    if (id_dep) {
+      loadAssignments(id_dep)
+    }
+  }, [])
+
+  // const assignments: Assignment[] = [
+  //   {
+  //     _id: '1',
+  //     Title: 'Math Homework',
+  //     Description: 'Complete exercises 1 to 10 from chapter 3.',
+  //     DueDate: '2025-01-15',
+  //     Attachments: 'math_homework.pdf'
+  //   },
+  //   {
+  //     _id: '2',
+  //     Title: 'Science Project',
+  //     Description: 'Prepare a presentation on the solar system.',
+  //     DueDate: '2025-01-20',
+  //     Attachments: 'science_project.zip'
+  //   },
+  //   {
+  //     _id: '3',
+  //     Title: 'History Essay',
+  //     Description: 'Write an essay on the Industrial Revolution.',
+  //     DueDate: '2025-01-18',
+  //     Attachments: 'history_essay.docx'
+  //   },
+  //   {
+  //     _id: '4',
+  //     Title: 'English Literature Analysis',
+  //     Description: "Analyze the themes in 'To Kill a Mockingbird'.",
+  //     DueDate: '2025-01-22',
+  //     Attachments: 'literature_analysis.docx'
+  //   },
+  //   {
+  //     _id: '5',
+  //     Title: 'Physics Lab Report',
+  //     Description: "Submit the lab report on Newton's Laws of Motion.",
+  //     DueDate: '2025-01-25',
+  //     Attachments: 'physics_lab_report.pdf'
+  //   }
+  // ]
 
   return (
     <Layout>
