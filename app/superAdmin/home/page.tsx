ba927095fd876fcd2dc0f203d093e6037fcbd168
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2025-01-04 23:09:54 +0100

    testing : Assignment in the admin part with risque 6

--- app/superAdmin/home/page.tsx
+++ app/superAdmin/home/page.tsx
@@ -15,9 +15,6 @@ export default function Page() {
 
   const id_dep = '670792e3ee0e13424434d371'
 
-  const [currentPage, setCurrentPage] = useState(1)
-  const itemsPerPage = 5
-
   const loadAssignments = throttle(async () => {
     if (id_dep) {
       await fetchAssignments(id_dep)
@@ -28,49 +25,25 @@ export default function Page() {
     loadAssignments()
   }, [id_dep, fetchAssignments])
 
-  const indexOfLastItem = currentPage * itemsPerPage
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage
-  const currentAssignments =
-    assignments?.slice(indexOfFirstItem, indexOfLastItem) || []
-
-  const handlePageChange = newPage => setCurrentPage(newPage)
-
   return (
     <Layout>
-      <div className='container mx-auto mt-32'>
-        <div className='grid grid-cols-4 gap-4 px-10'>
-          <div className='col-span-3'>
-            {currentAssignments.length > 0 ? (
-              currentAssignments.map((assignment, index) => (
-                <Grid item xs={12} key={assignment._id || index}>
-                  <div>
-                    <p>{assignment.Title}</p>
-                    <p>{assignment.DueDate}</p>
-                    <p>{assignment.Description}</p>
-                  </div>
-                </Grid>
-              ))
-            ) : (
-              <div className='flex h-full flex-col items-center justify-center'>
-                <Empty />
-                <h3 className='mb-4 mt-4 font-mono text-xl'>
-                  No Assignments Found
-                </h3>
+      <div>
+        {assignments.length > 0 ? (
+          assignments.map((assignment, index) => (
+            <Grid item xs={12} key={assignment._id || index}>
+              <div>
+                <p>{assignment.Title}</p>
+                <p>{assignment.DueDate}</p>
+                <p>{assignment.Description}</p>
               </div>
-            )}
+            </Grid>
+          ))
+        ) : (
+          <div>
+            <Empty />
+            <h3>No Assignments Found</h3>
           </div>
-          <div className='col-span-1'>
-            <Accordion />
-          </div>
-          <div className='col-start-1 col-end-4 mb-5 mt-5 self-center justify-self-center'>
-            <PaginationComponent
-              currentPage={currentPage}
-              totalItems={assignments?.length || 0}
-              itemsPerPage={itemsPerPage}
-              onPageChange={handlePageChange}
-            />
-          </div>
-        </div>
+        )}
       </div>
     </Layout>
   )
