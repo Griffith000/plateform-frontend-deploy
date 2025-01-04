a4947a6d569360924b3bd4b0b64a66fd5dcf3671
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2025-01-04 23:23:26 +0100

    testing : Assignment in the admin part with static data

--- app/superAdmin/home/page.tsx
+++ app/superAdmin/home/page.tsx
@@ -6,22 +6,53 @@ import Empty from '@/mic-component/lottie_animation/Empty'
 
 import Layout from '@/mic-component/Admin_UI/Layout/Layout'
 
-export default function Page() {
-  const assignments = useAssignmentStore(state => state.assignments)
-  const fetchAssignments = useAssignmentStore(state => state.fetchAssignments)
+export type Assignment = {
+  _id: string
+  Title: string
+  Description: string
+  DueDate: string
+  Attachments: string
+}
 
-  const id_dep = '670792e3ee0e13424434d371'
-
-  const loadAssignments = async () => {
-    if (id_dep) {
-      await fetchAssignments(id_dep)
-    }
+export const assignments: Assignment[] = [
+  {
+    _id: '1',
+    Title: 'Math Homework',
+    Description: 'Complete exercises 1 to 10 from chapter 3.',
+    DueDate: '2025-01-15',
+    Attachments: 'math_homework.pdf'
+  },
+  {
+    _id: '2',
+    Title: 'Science Project',
+    Description: 'Prepare a presentation on the solar system.',
+    DueDate: '2025-01-20',
+    Attachments: 'science_project.zip'
+  },
+  {
+    _id: '3',
+    Title: 'History Essay',
+    Description: 'Write an essay on the Industrial Revolution.',
+    DueDate: '2025-01-18',
+    Attachments: 'history_essay.docx'
+  },
+  {
+    _id: '4',
+    Title: 'English Literature Analysis',
+    Description: "Analyze the themes in 'To Kill a Mockingbird'.",
+    DueDate: '2025-01-22',
+    Attachments: 'literature_analysis.docx'
+  },
+  {
+    _id: '5',
+    Title: 'Physics Lab Report',
+    Description: "Submit the lab report on Newton's Laws of Motion.",
+    DueDate: '2025-01-25',
+    Attachments: 'physics_lab_report.pdf'
   }
+]
 
-  useEffect(() => {
-    loadAssignments()
-  }, [])
-
+export default function Page() {
   return (
     <Layout>
       <div>
