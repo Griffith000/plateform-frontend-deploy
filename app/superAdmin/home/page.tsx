33e34bdadf0588f104dd24a908db49a0615b9467
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2025-01-04 22:54:04 +0100

    testing : Assignment in the admin part with risque 6

--- app/superAdmin/home/page.tsx
+++ app/superAdmin/home/page.tsx
@@ -1,70 +1,53 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-
-import { Button, Grid, Typography } from '@mui/material'
-import { useRouter, useSearchParams } from 'next/navigation'
+import { Grid } from '@mui/material'
+import { useSearchParams } from 'next/navigation'
 import { useAssignmentStore } from '@/app/store/MyStore/AssignmentsStore'
-import AssignmentCard from '@/mic-component/assignment_UI/AssignmentCard'
 import Empty from '@/mic-component/lottie_animation/Empty'
 import PaginationComponent from '@/mic-component/PaginationComponent/PaginationComponent'
 import Accordion from '@/mic-component/InstructorAccordion/Accordion'
 import throttle from 'lodash.throttle'
-import { useAuthStore } from '@/store/MyStore/AuthStore'
 import Layout from '@/mic-component/Admin_UI/Layout/Layout'
 
 export default function Page() {
   const assignments = useAssignmentStore(state => state.assignments)
   const fetchAssignments = useAssignmentStore(state => state.fetchAssignments)
-  // const searchParams = useSearchParams()
-  // const id_dep = searchParams.get('id_dep')
-
-  const id_dep = '670792e3ee0e13424434d371'
+  const searchParams = useSearchParams()
+  const id_dep = searchParams.get('id_dep')
 
   const [currentPage, setCurrentPage] = useState(1)
   const itemsPerPage = 5
 
-  useEffect(() => {
-    const loadAssignments = throttle(async () => {
+  const loadAssignments = throttle(async () => {
+    if (id_dep) {
       await fetchAssignments(id_dep)
-    }, 1000)
+    }
+  }, 1000)
 
+  useEffect(() => {
     loadAssignments()
-  }, [])
+  }, [id_dep, fetchAssignments])
 
-  // Calculer les assignments à afficher pour la page actuelle
   const indexOfLastItem = currentPage * itemsPerPage
   const indexOfFirstItem = indexOfLastItem - itemsPerPage
-  const currentAssignments = assignments
-    ? assignments.slice(indexOfFirstItem, indexOfLastItem)
-    : []
+  const currentAssignments =
+    assignments?.slice(indexOfFirstItem, indexOfLastItem) || []
 
-  const handlePageChange = newPage => {
-    setCurrentPage(newPage)
-  }
+  const handlePageChange = newPage => setCurrentPage(newPage)
 
   return (
     <Layout>
-      <p> bonjour</p>
       <div className='container mx-auto mt-32'>
         <div className='grid grid-cols-4 gap-4 px-10'>
           <div className='col-span-3'>
             {currentAssignments.length > 0 ? (
-              currentAssignments.map(assignment => (
-                <Grid item xs={12} key={assignment._id}>
+              currentAssignments.map((assignment, index) => (
+                <Grid item xs={12} key={assignment._id || index}>
                   <div>
                     <p>{assignment.Title}</p>
                     <p>{assignment.DueDate}</p>
                     <p>{assignment.Description}</p>
                   </div>
-                  {/* <AssignmentCard
-                    assignment={{
-                      _id: assignment._id,
-                      Title: assignment.Title,
-                      DueDate: assignment.DueDate,
-                      description: assignment.Description,
-                      Attachments: null
-                    }}
-                  /> */}
                 </Grid>
               ))
             ) : (
@@ -82,7 +65,7 @@ export default function Page() {
           <div className='col-start-1 col-end-4 mb-5 mt-5 self-center justify-self-center'>
             <PaginationComponent
               currentPage={currentPage}
-              totalItems={assignments ? assignments.length : 0}
+              totalItems={assignments?.length || 0}
               itemsPerPage={itemsPerPage}
               onPageChange={handlePageChange}
             />
