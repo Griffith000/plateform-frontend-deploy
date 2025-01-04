5aaecaeba0848aade203b61d8cfd3e8ec39984ef
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2025-01-05 00:04:55 +0100

    testing : assignments in admin part 2

--- app/superAdmin/home/page.tsx
+++ app/superAdmin/home/page.tsx
@@ -3,28 +3,12 @@
 import Layout from '@/mic-component/Admin_UI/Layout/Layout'
 import React, { useEffect, useState } from 'react'
 
-import FullCalendar from '@fullcalendar/react'
-import dayGridPlugin from '@fullcalendar/daygrid'
-import { parse } from 'date-fns'
-import timeGridPlugin from '@fullcalendar/timegrid'
 import { Box, Grid, Modal, Typography } from '@mui/material'
-import { useSessionsStore } from '@/store/MyStore/SessionsStore'
-import { useSearchParams } from 'next/navigation'
+
 import { useAssignmentStore } from '@/store/MyStore/AssignmentsStore'
 import { Assignment } from '@/store/Models/Assignment'
 import AssignmentCard from '@/mic-component/assignment_UI/AssignmentCard'
 import Empty from '@/mic-component/lottie_animation/Empty'
-
-export type Session = {
-  _id: string
-  Title: string
-  Description: string
-  Instructor: string
-  InstructorId: string
-  Date: string
-  createdAt: string
-  Room: string
-}
 
 export default function Page() {
   const id_dep = '670792e3ee0e13424434d371'
@@ -45,18 +29,9 @@ export default function Page() {
       <div style={{ marginTop: '20px', padding: '10px' }}>
         {assignments.length > 0 ? (
           assignments.map(assignment => (
-            <Grid item xs={12} key={assignment._id}>
-              <AssignmentCard
-                assignment={{
-                  _id: assignment._id,
-                  Title: assignment.Title,
-                  DueDate: assignment.DueDate,
-                  description: assignment.Description,
-                  Attachments: null
-                }}
-              />
-              *
-            </Grid>
+            <>
+              <div>{assignment.Title}</div>
+            </>
           ))
         ) : (
           <div className='flex h-full flex-col items-center justify-center'>
