a1497cc7462dd880ac5c687e72d9bd11153639c0
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2025-01-04 23:16:57 +0100

    testing : Assignment in the admin part with risque 6

--- app/superAdmin/home/page.tsx
+++ app/superAdmin/home/page.tsx
@@ -1,12 +1,9 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import { Grid } from '@mui/material'
-import { useSearchParams } from 'next/navigation'
+import Grid from '@mui/material/Grid2'
 import { useAssignmentStore } from '@/app/store/MyStore/AssignmentsStore'
 import Empty from '@/mic-component/lottie_animation/Empty'
-import PaginationComponent from '@/mic-component/PaginationComponent/PaginationComponent'
-import Accordion from '@/mic-component/InstructorAccordion/Accordion'
-import throttle from 'lodash.throttle'
+
 import Layout from '@/mic-component/Admin_UI/Layout/Layout'
 
 export default function Page() {
@@ -15,22 +12,22 @@ export default function Page() {
 
   const id_dep = '670792e3ee0e13424434d371'
 
-  const loadAssignments = throttle(async () => {
+  const loadAssignments = async () => {
     if (id_dep) {
       await fetchAssignments(id_dep)
     }
-  }, 1000)
+  }
 
   useEffect(() => {
     loadAssignments()
-  }, [id_dep, fetchAssignments])
+  }, [])
 
   return (
     <Layout>
       <div>
         {assignments.length > 0 ? (
           assignments.map((assignment, index) => (
-            <Grid item xs={12} key={assignment._id || index}>
+            <Grid key={assignment._id || index}>
               <div>
                 <p>{assignment.Title}</p>
                 <p>{assignment.DueDate}</p>
