bc44fc1978527c393e41a5acd55cda4a3b711d0a
MariemTlatli <127855946+MariemTlatli@users.noreply.github.com>
2025-01-05 02:26:28 +0100

    testing : home changed in admin

--- app/superAdmin/home/page.tsx
+++ app/superAdmin/home/page.tsx
@@ -1,74 +1,11 @@
 'use client'
-
 import Layout from '@/mic-component/Admin_UI/Layout/Layout'
-import React, { useEffect, useState } from 'react'
 
-import { useAssignmentStore } from '@/store/MyStore/AssignmentsStore'
-import { Assignment } from '@/store/Models/Assignment'
-import Empty from '@/mic-component/lottie_animation/Empty'
-import AssignmentCard from '@/mic-component/assignment_UI/AssignmentCard'
-import PaginationComponent from '@/mic-component/PaginationComponent/PaginationComponent'
-
-export default function Page() {
-  const id_dep = '670792e3ee0e13424434d371'
-  const fetchAssignments = useAssignmentStore(state => state.fetchAssignments)
-
-  const assignments: Assignment[] = useAssignmentStore(
-    state => state.assignments
-  )
-
-  const [currentPage, setCurrentPage] = useState(1)
-  const itemsPerPage = 5
-
-  const indexOfLastItem = currentPage * itemsPerPage
-  const indexOfFirstItem = indexOfLastItem - itemsPerPage
-  const currentAssignments = assignments
-    ? assignments.slice(indexOfFirstItem, indexOfLastItem)
-    : []
-
-  const handlePageChange = newPage => {
-    setCurrentPage(newPage)
-  }
-
-  useEffect(() => {
-    const loadAssignments = async (departmentId: string) => {
-      await fetchAssignments(departmentId)
-    }
-    if (id_dep) {
-      loadAssignments(id_dep)
-    }
-  }, [])
-
+export default function AddUserForm() {
   return (
     <Layout>
-      <div style={{ marginTop: '20px', padding: '10px' }}>
-        {assignments.length > 0 ? (
-          assignments.map(assignment => (
-            <div key={assignment._id}>
-              <AssignmentCard
-                assignment={{
-                  _id: assignment._id,
-                  Title: assignment.Title,
-                  DueDate: assignment.DueDate,
-                  description: assignment.Description,
-                  Attachments: null
-                }}
-              />
-            </div>
-          ))
-        ) : (
-          <div>
-            <h3>No Assignments Found</h3>
-          </div>
-        )}
-        <div>
-          <PaginationComponent
-            currentPage={currentPage}
-            totalItems={assignments ? assignments.length : 0}
-            itemsPerPage={itemsPerPage}
-            onPageChange={handlePageChange}
-          />
-        </div>
+      <div>
+        <p>admin Home Pas encore implémenter </p>
       </div>
     </Layout>
   )
